c6f4500578f254610a04924072b5a8ceefa966ae
hyeon <hyon5807@gmail.com>
2026-03-11 16:24:42 +0900

    pm3-11 update

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Slf4j
@@ -37,20 +39,35 @@ public class UserRestController {
         return userRepository.findAll();
     }
 
-    //Id로 목록조회
+    //Id로 User 조회
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
-        User existUser = userRepository.findById(id) //Optional<User>
+        Optional<User> optionalUser = userRepository.findById(id);
+        User existUser = getExistUser(optionalUser);
+        return existUser;
+    }
+
+    //Email 조회하고 User 수정하기
+    @PatchMapping("/{email}/")
+    public User updateUser(@PathVariable String email, @RequestBody User userDetail){
+        User existUser = getExistUser(userRepository.findByEmail(email));
+        existUser.setName(userDetail.getName());
+        return userRepository.save(existUser);
+    }
+
+    //User 삭제하기
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        User user = getExistUser(optionalUser);
+        userRepository.delete(user);
+        return ResponseEntity.ok("Id = " + id + " User가 삭제 되었습니다.");
+    }
+    //private 공통 메서드
+    private static User getExistUser(Optional<User> optionalUser) {
+        User existUser = optionalUser //Optional<User>
                 .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));//User
         return existUser;
     }
 
-    //Email 조회하고 수정하기
-    @PatchMapping("/{email}/")
-    public User updateUser(@PathVariable String email, @RequestBody User userDetail) {
-        User existUser = userRepository.findByEmail(email)
-                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
-        existUser.setName(userDetail.getName());
-        return userRepository.save(existUser);
-    }
 }
