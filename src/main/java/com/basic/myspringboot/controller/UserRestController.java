7a95d632f22ac1ef350373b6f73fcb7a1f08474e
hyeon <hyon5807@gmail.com>
2026-03-11 11:09:34 +0900

    am3-11 update

--- src/main/java/com/basic/myspringboot/controller/UserRestController.java
+++ src/main/java/com/basic/myspringboot/controller/UserRestController.java
@@ -1,11 +1,13 @@
 package com.basic.myspringboot.controller;
 
 import com.basic.myspringboot.entity.User;
+import com.basic.myspringboot.exception.BusinessException;
 import com.basic.myspringboot.repository.CustomerRepository;
 import com.basic.myspringboot.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,4 +37,20 @@ public class UserRestController {
         return userRepository.findAll();
     }
 
+    //Id로 목록조회
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        User existUser = userRepository.findById(id) //Optional<User>
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));//User
+        return existUser;
+    }
+
+    //Email 조회하고 수정하기
+    @PatchMapping("/{email}/")
+    public User updateUser(@PathVariable String email, @RequestBody User userDetail) {
+        User existUser = userRepository.findByEmail(email)
+                .orElseThrow(() -> new BusinessException("User Not Found", HttpStatus.NOT_FOUND));
+        existUser.setName(userDetail.getName());
+        return userRepository.save(existUser);
+    }
 }
