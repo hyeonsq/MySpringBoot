90a4e8cc4c5dd1546f67e928afcc1e75d9dd6e6b
hyeon <hyon5807@gmail.com>
2026-03-13 13:13:05 +0900

    update

--- src/main/java/com/basic/myspringboot/security/userinfo/UserInfoController.java
+++ src/main/java/com/basic/myspringboot/security/userinfo/UserInfoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/userinfos")
+@RequestMapping("/userinfos")
 public class UserInfoController {
     @Autowired
     private UserInfoUserDetailsService service;
